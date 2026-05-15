17d6c34e8d9616ac93e185c567266e1aee9312ba
pSiewiera <pawel.k.siewiera@gmail.com>
2026-05-15 12:21:12 +0200

    dodanie pomiarów dystansu miast

--- Lab5/main.py
+++ Lab5/main.py
@@ -9,3 +9,11 @@ class SA_TS:
         self.path = list(range(num_nodes))
         random.shuffle(self.path)
         self.path.append(self.path[0])
+
+    def get_dist(self, a, b):
+        """Odległość miedzy mastami a i b"""
+        node_a = self.nodes[a]
+        node_b = self.nodes[b]
+        dx = node_a[0] - node_b[0]
+        dy = node_a[1] - node_b[1]
+        return math.floor(0.5 + math.sqrt(dx*dx + dy*dy))
